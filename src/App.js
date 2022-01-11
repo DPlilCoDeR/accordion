22eda02e9533070da6b87651cc6eb73b66786e43
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-01-11 16:06:34 +0100

    add final styles 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   <main>
     <div className='container'>
       <h3>Questions and answers about login</h3>
-      <section className='info'>
+      <section>
         {questions.map(question => {
           return <SingleQuestion key={question.id} question={question}/>
         })}
